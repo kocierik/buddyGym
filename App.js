50148465b5fc36e78e817ff62c563c6bbb307ef8
kocierik <kocierik@gmail.com>
2022-04-12 19:48:46 +0200

    chore: add button

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <Text style={styles.textField}> ciao cioa</Text>
+        <Button title="ciao" />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#000018",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textField: {
+    color: "#fff",
+  }
 });
