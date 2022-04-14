a95d4c313b84d75d24ea785f61936d86e353874f
kocierik <kocierik@gmail.com>
2022-04-14 16:18:07 +0200

    feat: add social Icon

--- App.js
+++ App.js
@@ -11,32 +11,58 @@ import {
 import Login from './src/Login'
 import images from './assets/Allimages'
 import Theme from './assets/colors'
+import { AntDesign, FontAwesome } from '@expo/vector-icons'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export default function App() {
   // https://coolors.co/palette/000814-001d3d-003566-ffc300-ffd60a PALETTE
 
   return (
     <NativeBaseProvider theme={Theme}>
-      <Box
-        style={styles.container}
-        bg="erik.background"
-        fontFamily={'Montserrat-VariableFont_wght'}
+      <KeyboardAwareScrollView
+        resetScrollToCoords={{ x: 0, y: 0 }}
+        scrollEnabled={false}
       >
         <Box
-          flex={1}
-          alignItems={'center'}
-          justifyContent={'flex-end'}
-          padding={10}
+          style={styles.container}
+          bg="erik.background"
+          fontFamily={'Montserrat-VariableFont_wght'}
         >
-          <Image source={images.login} alt="Logo" height={100} width={100} />
+          <Box
+            flex={1}
+            alignItems={'center'}
+            justifyContent={'flex-end'}
+            padding={10}
+          >
+            <Image source={images.login} alt="Logo" height={100} width={100} />
+          </Box>
+          <Box style={styles.content}>
+            <Text color="erik.text" p={10} fontSize="4xl" fontWeight={'bold'}>
+              Login
+            </Text>
+            <Login />
+          </Box>
+          <Box
+            flex={1}
+            alignItems={'center'}
+            flexDir={'row'}
+            justifyContent={'center'}
+          >
+            <AntDesign
+              name="facebook-square"
+              size={30}
+              color="black"
+              style={{ padding: 5 }}
+            />
+            <FontAwesome
+              name="twitter-square"
+              size={30}
+              color="black"
+              style={{ padding: 5 }}
+            />
+          </Box>
         </Box>
-        <Box style={styles.content}>
-          <Text color="erik.text" p={10} fontSize="4xl" fontWeight={'bold'}>
-            Login
-          </Text>
-          <Login />
-        </Box>
-      </Box>
+      </KeyboardAwareScrollView>
     </NativeBaseProvider>
   )
 }
